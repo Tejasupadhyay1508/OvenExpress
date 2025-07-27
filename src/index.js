76210579fe2ca60bbf3d56aeefdd5ac98e4714f2
Tejas <tejasupadhyay48@gmail.com>
2025-07-27 16:27:32 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const ServerConfig = require('./config/ServerConfig');
 const connectDB = require('./config/dbconfig');
 
 const app = express();
-
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
 
 app.listen(ServerConfig.PORT, async ()=>{
     await connectDB();
